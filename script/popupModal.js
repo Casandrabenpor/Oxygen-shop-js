dc9136d1a4e87a7858552102ccd0fd2922bcec10
casandrabenpor <casandra211@gmail.com>
2023-03-05 18:01:19 +0100

    Trabajo final oxygen shop Casandra

--- script/popupModal.js
+++ script/popupModal.js
@@ -1,32 +1,36 @@
-(function () {
-    let updateButton = document.getElementById('updateDetails');
-    let cancelButton = document.getElementById('cancel');
-    let favDialog = document.getElementById('favDialog');
-     // El botón Actualizar abre un cuadro de diálogo modal después de 5 segundos
-    setTimeout(function(){
-            favDialog.showModal();
-            }, 5000);
-  
-    // El botón de cancelar formulario cierra el cuadro de diálogo
-    cancelButton.addEventListener('click', function () {
-      favDialog.close();
-    });
-  
-    // Agregamos un evento "submit" al formulario para validar el campo de correo electrónico
-    favDialog.querySelector('form').addEventListener('submit', function(e) {
-      const mailPopup = this.elements.mailPopup;
-      const mailPopupRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-      if (!mailPopupRegExp.test(mailPopup.value)) {
-        mailPopup.classList.add("invalid");
-        mailPopup.nextElementSibling.innerText = "I expect an e-mail, darling!";
-        mailPopup.nextElementSibling.classList.add("error", "active");
-        alert("You have entered an invalid email address!");
-        e.preventDefault();
-      } else {
-        // Si el correo electrónico es válido, mostramos el cuadro de diálogo
-        favDialog.showModal();
-      }
-    });
-  })();
-  
+let updateButton = document.getElementById('updateDetails');
+let cancelButton = document.getElementById('cancel');
+let favDialog = document.getElementById('favDialog');
+// El botón Actualizar abre un cuadro de diálogo modal después de 5 segundos
+setTimeout(function () {
+  const storage = window.sessionStorage;
+  console.log(storage.getItem("visit"));
+  if (storage.getItem("visit") == null) {
+    favDialog.showModal();
+    storage.setItem("visit", true);
+  }
+}, 5000);
+
+// El botón de cancelar formulario cierra el cuadro de diálogo
+cancelButton.addEventListener('click', function () {
+  favDialog.close();
+});
+
+// Agregamos un evento "submit" al formulario para validar el campo de correo electrónico
+favDialog.querySelector('form').addEventListener('submit', function (e) {
+  const mailPopup = this.elements.mailPopup;
+  const mailPopupRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!mailPopupRegExp.test(mailPopup.value)) {
+    mailPopup.classList.add("invalid");
+    mailPopup.nextElementSibling.innerText = "I expect an e-mail, darling!";
+    mailPopup.nextElementSibling.classList.add("error", "active");
+    alert("You have entered an invalid email address!");
+    e.preventDefault();
+  } else {
+    // Si el correo electrónico es válido, mostramos el cuadro de diálogo
+    favDialog.showModal();
+  }
+});
+
+
--- script/pricing.js
+++ script/pricing.js
@@ -4,9 +4,12 @@ let basicUsd = 0;
 let professionalUsd = 25;
 let premiumUsd = 60;
 
+
 let gbpButton = document.getElementById("gbp");
 gbpButton.addEventListener('click', function () {
+    //devuelve la promesa del factor de cambio de libras
     let currencyChange = getData("gbp");
+    //modigica precio
     modifyPrice(currencyChange, "£");
 });
 
@@ -23,30 +26,33 @@ usdButton.addEventListener('click', function (
         let price1 = document.getElementById("price1");
         let price2 = document.getElementById("price2");
         let price3 = document.getElementById("price3");
-        price1.innerHTML = `$ ${basicUsd} `;
-        price2.innerHTML = `$ ${professionalUsd} `;
-        price3.innerHTML = `$ ${premiumUsd} `;
+        //cambia precio con el valor por defecto de arriba
+        price1.innerHTML = `$${basicUsd} `;
+        price2.innerHTML = `$${professionalUsd} `;
+        price3.innerHTML = `$${premiumUsd} `;
     
-
+  
 });
 
 
 
 //currencySymbol = € / $ / £
 const modifyPrice = async (currencyChangeAsync, currencySymbol) =>{
+    // promesa que espera la respuesta de la api
     let currencyChange = await currencyChangeAsync;
-
+//Marh.round es para redondear el precio.
+//Variable que multiplica el precio por defecto con el cambio de moneda,currencyChange es el numero que se recoge de la api
     let basicPrice = Math.round (currencyChange * basicUsd);
     let professionalPrice = Math.round( currencyChange * professionalUsd);
     let premiumPrice =  Math.round(currencyChange * premiumUsd);
-
+//obtienes los elementos html de cada uno de los precios
     let price1 = document.getElementById("price1");
     let price2 = document.getElementById("price2");
     let price3 = document.getElementById("price3");
-
-    price1.innerHTML = `${currencySymbol} ${basicPrice}`;
-    price2.innerHTML = `${currencySymbol} ${professionalPrice}`;
-    price3.innerHTML = `${currencySymbol} ${premiumPrice}`;
+// actualizar los precios, el simbolo y el precio que se ha calculado
+    price1.innerHTML = `${currencySymbol}${basicPrice}`;
+    price2.innerHTML = `${currencySymbol}${professionalPrice}`;
+    price3.innerHTML = `${currencySymbol}${premiumPrice}`;
    
 };
 
@@ -57,7 +63,7 @@ const getData = async (currency) => {
         const response = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json');
         const responseData = await response.json();
 
-
+        // si el cambio es igual a eur, devolvemos usd a eur y sino usd a gbp
         if (currency === "eur") {
             return responseData.usd.eur;
         } else {
